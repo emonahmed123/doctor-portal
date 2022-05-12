ad988b831d79ec458d92d9860ae3965be3458d48
emonahmed123 <imonshomon@gmail.com>
2022-05-11 18:10:28 -0700

    added modal and edit

--- src/pages/Appoinment/AvailableAppointments.js
+++ src/pages/Appoinment/AvailableAppointments.js
@@ -1,10 +1,30 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import BookingModal from './BookingModal';
+import Service from './Service';
+
 
 const AvailableAppointments = ({date}) => {
-    return (
-        <div>
-              Available Appoinments on {format(date, 'PP') }
+   const [services , setServices] =useState( [ ]);
+  const [ treatment, setTreatment] =useState(null)
+   useEffect(()=>{
+         fetch(' service.json ')
+         .then(res=>res.json())
+         .then(data =>setServices(data))
+
+   },[])
+   return (
+        <div >
+                 <h4 className=' text-xl text-primary text-center font-bold'> Available Appoinments on {format(date, 'PP') }</h4>
+              
+              <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-10 '> 
+          {
+             services.map(service => <Service key={service._id} service={service} setTreatment={setTreatment}> </Service>)
+            
+            } 
+              </div>
+              {treatment && <BookingModal   date={date}  treatment={treatment}
+                setTreatment={setTreatment}></BookingModal> }
         </div>
     );
 };
---
+++ src/pages/Appoinment/BookingModal.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { format } from 'date-fns';
+const BookingModal = ({treatment,date}) => {
+   const {name ,slots}=treatment;
+    return (
+        <div>
+            <input type="checkbox" id="booking-modal" class="modal-toggle" />
+<div class="modal modal-bottom sm:modal-middle">
+  <div class="modal-box">
+  <label for="booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+    <h3 class="font-bold text-lg text-secondary">{name}</h3>
+       <form  className=' grid grid-cols-1 gap-3 justify-items-center mt-2' >
+       <input type="text"  disabled value={format(date, 'PP')} class="input input-bordered w-full max-w-xs" />
+       <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+       <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+       <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+       <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+       <input type="submit"  value="submit" class="btn btn-secondary w-full max-w-xs" />
+      
+       </form>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default BookingModal;
